1b7928f22dd9a078bd8cbb12394878de1258e7d3
deriverge s.r.o. <158823782+deriverge@users.noreply.github.com>
2024-10-06 12:30:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ $(".hamburger").click(function(){
     $("#mobileMenuPage").stop(true, true).slideToggle(300);
 });
 
+//scroll top button start
+const scrollButton = document.getElementById("scrollTopButton");
+let setPosition = 200;
+
+window.addEventListener("scroll", function() {
+    
+    let currentPosition = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentPosition > setPosition){
+        scrollButton.style.opacity = "1";
+    }
+    else{
+        scrollButton.style.opacity = "0";
+    }
+});
+
+function nahoru() {
+    console.log("scroll top aktivovan");
+    window.scrollTo({top: 0, behavior: "smooth"});
+}
+
+
+//scroll top button konec
+
